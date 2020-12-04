48eceb0ed2c84e3514f154f6a661d12913445bad
DoraymonIT <bendrimou@gmail.com>
2020-12-04 12:55:11 +0100

    images

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -117,49 +117,49 @@
         });
       });
     } else {
-      php_email_form_submit(this_form,action,this_form.serialize());
+      // php_email_form_submit(this_form,action,this_form.serialize());
     }
     
     return true;
   });
 
-  function php_email_form_submit(this_form, action, data) {
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: data,
-      timeout: 40000
-    }).done( function(msg){
-      if (msg.trim() == 'OK') {
-        this_form.find('.loading').slideUp();
-        this_form.find('.sent-message').slideDown();
-        this_form.find("input:not(input[type=submit]), textarea").val('');
-      } else {
-        this_form.find('.loading').slideUp();
-        if(!msg) {
-          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
-        }
-        this_form.find('.error-message').slideDown().html(msg);
-      }
-    }).fail( function(data){
-      console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
-    });
-  }
+  // function php_email_form_submit(this_form, action, data) {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: action,
+  //     data: data,
+  //     timeout: 40000
+  //   }).done( function(msg){
+  //     if (msg.trim() == 'OK') {
+  //       this_form.find('.loading').slideUp();
+  //       this_form.find('.sent-message').slideDown();
+  //       this_form.find("input:not(input[type=submit]), textarea").val('');
+  //     } else {
+  //       this_form.find('.loading').slideUp();
+  //       if(!msg) {
+  //         msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+  //       }
+  //       this_form.find('.error-message').slideDown().html(msg);
+  //     }
+  //   }).fail( function(data){
+  //     console.log(data);
+  //     var error_msg = "Form submission failed!<br>";
+  //     if(data.statusText || data.status) {
+  //       error_msg += 'Status:';
+  //       if(data.statusText) {
+  //         error_msg += ' ' + data.statusText;
+  //       }
+  //       if(data.status) {
+  //         error_msg += ' ' + data.status;
+  //       }
+  //       error_msg += '<br>';
+  //     }
+  //     if(data.responseText) {
+  //       error_msg += data.responseText;
+  //     }
+  //     this_form.find('.loading').slideUp();
+  //     this_form.find('.error-message').slideDown().html(error_msg);
+  //   });
+  // }
 
 })(jQuery);
